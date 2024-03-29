979a470b8d5b2ccf7b666e29fe85af0e7eeafa05
T_N <terannovonsaker@gmail.com>
2024-03-28 19:49:40 -0500

    changed something

--- src/components/notfound/NotFound.js
+++ src/components/notfound/NotFound.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const NotFound = () => {
-  return <div className="section-wrapper">404: Nothing Here</div>;
+  return <div className="section-wrapper">404 Nothing Here</div>;
 };
 
 export default NotFound;
